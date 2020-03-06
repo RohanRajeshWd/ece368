e001a33fc5edbc6eec699930a7966b59ffa6d9bc
Brown-Mamba <rrajesh@purdue.edu>
2020-03-05 23:09:56 -0500

    started

---
+++ proj3/packing.c
@@ -0,0 +1,5 @@
+//packing functions use an array to bring in the function
+
+
+#include<stdio.h>
+void input(filename, 
---
+++ proj3/packing.h
@@ -0,0 +1,11 @@
+#define__PACKING.H__
+
+struct RecNode{
+  int len;
+  int width;
+  int label;
+  char type;
+  struct recNode * left;
+  struct recNode * right;
+}
+
