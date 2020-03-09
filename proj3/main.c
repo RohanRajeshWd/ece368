7fcf939223261befac6a657f915292a41685ae52
Brown-Mamba <rrajesh@purdue.edu>
2020-03-09 10:40:05 -0400

    I/O complete

--- proj3/main.c
+++ proj3/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include<string.h>
+#include<ctype.h>
 int main(int argc, char *argv[]){
   FILE*fp;
   int size;
@@ -20,51 +22,76 @@ int main(int argc, char *argv[]){
       size++;
   }
   fseek(fp,0,SEEK_SET);
-  //typeArr[size-1] = lenArr[size-1] = wArr[size-1] = lArr[size-1]=fgetc(fp); //malloc this
+  /*calloc = typeArr[size-1] = wArr[size-1] = hArr[size-1] = lArr[size-1]=fgetc(fp); //malloc this*/
+  int *typeArr = (int*)calloc(size, sizeof(int));
+  int *hArr = (int*)calloc(size, sizeof(int));
+  int *wArr = (int*)calloc(size, sizeof(int));
+  
   fseek(fp,0,SEEK_SET);
   int line = 1;
   int i;
   
   for ( c = fgetc(fp); c !=EOF; c = fgetc(fp)){
-    if(c == '('){
-    inPar = 1;//open parenthises
-    }
-    if(c ==')'){
-      inPar = 0;//close parenthises
-      comma = 0;
-    }
+   
+    
     if(c == 'V'){ //case vert 
-      typeArr[size-line] = 1;
-      lenArr[size-line] = wArr[size-line] = lArr[size-line]= 
+      typeArr[size-line] = -1;
+      wArr[size-line] = hArr[size-line] = 0;
     }
     if(c == 'H'){ //case horiz
-      typeArr[size-line] = 2;
-      lenArr[size-line] = wArr[size-line] = lArr[size-line]= 
+      typeArr[size-line] = -2;
+      wArr[size-line] = hArr[size-line] = 0;
     }
     if(c == '\n'){
       //newLine
       line++;
     }
-    if( c == ','){
-      comma = 1;
-    }
+    
     if (isdigit(c) && inPar ==0){//label of leaf
-        num = stoi(c);
+        num = (c-'0');
+        c = fgetc(fp);
         while(c !='('){
-            i++
-            num = 10*num+stoi(c);
+            
+            num = 10*num+(c-'0');
+            c=fgetc(fp);
         }
     typeArr[size-line] = num;
     num = 0;
         
-    }   
+    }
+     if(c == '('){
+    inPar = 1;//open parenthises
+    }
     if((isdigit(c))&& inPar ==1 && comma == 0){//length
-    
+    num = (c-'0');
+    c = fgetc(fp);
+        while(c !=','){
+            
+            num = 10*num+(c-'0');
+            c=fgetc(fp);
+        }
+    wArr[size-line] = num;
+    num = 0;
         
     }
-    if((isdigit(c))&& inPar ==1 && comma == 1){//width
+    if( c == ','){
+      comma = 1;
     }
-
+    if((isdigit(c))&& inPar ==1 && comma == 1){//width
+        num = (c-'0');
+        c = fgetc(fp);
+        while(c !=')'){
+            
+            num = 10*num+(c-'0');
+            c=fgetc(fp);
+        }
+    hArr[size-line] = num;
+    num = 0;
+    }
+    if(c ==')'){
+      inPar = 0;//close parenthises
+      comma = 0;//close comma
+    }
 
   
   }
@@ -72,6 +99,13 @@ int main(int argc, char *argv[]){
 
 
   fclose(fp);
-  printf("%d \n",size);
-
+  printf("\n%d\n",size);
+  for (int j =0; j<size; j++){
+      printf("%d %d %d",typeArr[j], wArr[j], hArr[j]);
+      printf("\n");
+      /*printf(" %d  ", wArr[j]);
+      printf("\n");
+      printf(" %d  ", hArr[j]);
+      */
+  }
 }
